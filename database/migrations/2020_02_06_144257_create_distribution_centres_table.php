2eb1896417eee372fcbeed80e42b110ef55672aa
Juho Taipale <juho.taipale@vitabalans.fi>
2020-02-06 16:49:48 +0200

    Initial commit

---
+++ database/migrations/2020_02_06_144257_create_distribution_centres_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateDistributionCentresTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('distribution_centres', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('distribution_centres');
+    }
+}
