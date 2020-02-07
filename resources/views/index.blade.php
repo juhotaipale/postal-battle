964a3f77830d7a43e7c698468102929c2419ac2d
Juho Taipale <juho.taipale@vitabalans.fi>
2020-02-07 12:07:11 +0200

    Jaettu layout

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,17 +1,10 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('layout/app')
+@section('content')
 
-        <title>Laravel</title>
+    @auth
+        <pusher></pusher>
+    @elseguest
+        <login></login>
+    @endauth
 
-        <link rel="stylesheet" href="{{ asset('css/app.css') }}" />
-        <script type="text/javascript" src="{{ asset('js/app.js') }}" defer></script>
-    </head>
-    <body>
-        <div id="app">
-            <pusher></pusher>
-        </div>
-    </body>
-</html>
+@endsection
