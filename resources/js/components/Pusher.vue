b0ce678497daa80c211bd0fa44490a09c07420d6
Juho Taipale <juho.taipale@vitabalans.fi>
2020-02-07 10:33:40 +0200

    Lisätty ja testattu sokettiyhteydet Pusherilla, package-lock.json lisätty gitignoreen

---
+++ resources/js/components/Pusher.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">Pusher connection test</div>
+
+                    <div class="card-body">
+                        <ul>
+                            <li v-for="message in messages">
+                                {{ message }}
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                messages: []
+            }
+        },
+        mounted() {
+            Echo.channel('testing')
+                .listen('.test-event', (e) => {
+                    console.log(e);
+                    this.messages.push(JSON.stringify(e));
+                })
+        }
+    }
+</script>
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -11,7 +11,7 @@
     </head>
     <body>
         <div id="app">
-
+            <pusher></pusher>
         </div>
     </body>
 </html>
